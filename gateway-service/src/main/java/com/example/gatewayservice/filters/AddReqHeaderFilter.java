76c1901a0a6d15737ecd435eb1e1fe7da2a0d5cf
raocrisan <theworldismine249@yahoo.com>
2019-01-28 15:00:13 +0200

    First commit

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filters/AddReqHeaderFilter.java
@@ -0,0 +1,31 @@
+package com.example.gatewayservice.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+
+public class AddReqHeaderFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+
+        currentContext.addZuulRequestHeader("x-location", "Romania");
+        return null;
+    }
+}
